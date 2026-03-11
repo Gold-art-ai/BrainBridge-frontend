f09b5aa6969e4a0f600228a2e3415113acf71af4
Ishara Gold <isharagold6@gmail.com>
2026-03-11 21:59:59 +0200

    Remove .history folder and clean up workspace

--- .history/brainbridge/app/redux/ProviderClient_20260225205331.jsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './api/Features/store';
-
-export default function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
-}
