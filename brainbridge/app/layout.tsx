fedec80a6ea041395a6eb357b11c191067da86fb
NIYOBYOSE-Isaac-Precieux <isaprecieux112@gmail.com>
2026-03-14 20:51:30 +0200

    Login page modification, added username parameter

--- brainbridge/app/layout.tsx
+++ brainbridge/app/layout.tsx
@@ -1,33 +1,33 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import ReduxProvider from './redux/ProviderClient'
+  import type { Metadata } from "next";
+  import { Geist, Geist_Mono } from "next/font/google";
+  import "./globals.css";
+  import ReduxProvider from './redux/ProviderClient'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+  const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+  });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+  const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+  });
 
-export const metadata: Metadata = {
-  title: "BrainBridge",
-  description: "Imariyiki sosiyeti",
-};
+  export const metadata: Metadata = {
+    title: "BrainBridge",
+    description: "Imariyiki sosiyeti",
+  };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ReduxProvider>{children}</ReduxProvider>
-      </body>
-    </html>
-  );
-}
+  export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+          <ReduxProvider>{children}</ReduxProvider>
+        </body>
+      </html>
+    );
+  }
